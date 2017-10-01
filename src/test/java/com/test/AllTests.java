cd0c3ea86714284ba9d6cef1f88596cf794acc50
DESKTOP-5E79NDD\Asus <jerinbabukb@gmail.com>
2017-10-01 22:21:22 +0200

    testcase

---
+++ src/test/java/com/test/AllTests.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		return suite;
+	}
+
+}
---
+++ src/test/java/com/test/FirstTest.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class FirstTest extends TestCase {
+	
+@Test
+public void testFun() {
+	
+	int a = 5;
+	int b = 6;
+	int c= a+b;
+	assertTrue(c==11);
+}
+
+}
